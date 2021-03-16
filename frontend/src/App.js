914e13c30c53086ce9b91dd1ce86366073543b03
juno0810 <juno0810@naver.com>
2021-03-16 23:53:49 +0900

    S04P22A207-22 [FE+] Edit use styled-components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    // background-color: black;
+  };
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Navbar />
     </div>
   );
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,29 +1,64 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import Main from "../View/Main";
 import Recommendation from "../View/Recommendation";
 import MyList from "../View/MyList";
 import Profile from "../View/Profile";
+// import styled from 'styled-components';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    backgroundColor: "#cee6b4",
+    position: 'fixed',
+    bottom: 0,
+  },
+});
 
 function Navbar() {
+  const history = useHistory();
+  const classes = useStyles();
+  const [value, setValue] = React.useState('/');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    console.log(history);
+    history.push({
+      pathname: newValue,
+    });
+  };
+
   return (
     <Router>
-      <ul className="nav justify-content-around fixed-bottom mb-5">
+      {/* <ul className="nav nav-pills nav-justified justify-content-around fixed-bottom bg-dark">
         <li className="nav-item">
-          <Link to="/">메인</Link>
+          <Link to="/" className="nav-link active">메인</Link>
         </li>
         <li className="nav-item">
-          <Link to="/recommendation">추천만화</Link>
+          <Link to="/recommendation" className="nav-link">추천만화</Link>
         </li>
         <li className="nav-item">
-          <Link to="/mylist">내 리스트</Link>
+          <Link to="/mylist" className="nav-link">내 리스트</Link>
         </li>
         <li className="nav-item">
-          <Link to="/profile">프로필</Link>
+          <Link to="/profile" className="nav-link">프로필</Link>
         </li>
-      </ul>
-
-      <hr />
+      </ul> */}
+      <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+        <BottomNavigationAction label="메인" value="/" icon={<HomeIcon/>}></BottomNavigationAction>
+        <BottomNavigationAction label="추천만화" value="/recommendation" icon={<ThumbUpIcon/>}></BottomNavigationAction>
+        <BottomNavigationAction label="내 리스트" value="/mylist" icon={<BookmarksIcon/>}></BottomNavigationAction>
+        <BottomNavigationAction label="프로필" value="/profile" icon={<AccountBoxIcon/>}></BottomNavigationAction>
+      </BottomNavigation>
 
       <Switch>
         <Route exact path="/">
