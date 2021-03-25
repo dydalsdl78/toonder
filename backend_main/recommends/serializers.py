5fcd8ea8a7e8b2c5136cc606e7a9747cdf9d1c70
현성섭 <gustjdtjq@gmail.com>
2021-03-25 15:33:06 +0900

    [+BE] Create BE/main_server_recomm branch

---
+++ backend_main/recommends/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Webtoon
+
+
+class WebtoonSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Webtoon
+    fields = '__all__'
--- backend_main/recommends/views.py
+++ backend_main/recommends/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Webtoon
+from .serializers import WebtoonSerializer
 
+
+# 
 
 def index(request):
-    pass
+    webtoons = Webtoon.objects.all()
+
+
+    print(type(webtoons))
+    print(webtoons) # 전체 웹툰의 QuerySet
+    print(webtoons[1]) # 해당 웹툰의 QuerySet
+    print(webtoons[1].webtoon_name) # 해당 웹툰의 이름
+
+    return 0
