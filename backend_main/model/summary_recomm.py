63e6e5dd70412dcebdae5d46f48aa4da31e1ca5a
dydalsdl78 <dydalsdl78@gmail.com>
2021-03-26 10:31:47 +0900

    S04P23A207-34 [BE+] Edit summary_recommend logic in views.py

--- backend_main/model/summary_recomm.py
+++ backend_main/model/summary_recomm.py
@@ -8,6 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 warnings.filterwarnings('ignore')
 
+# 데이터프레임화
 def to_dataframe(data):
   
   ls_webtoon = []
@@ -28,23 +29,25 @@ def to_dataframe(data):
   
   return webtoon_df
 
+# 토큰화 및 tf-idf 행렬 계산
 def tokenizer(webtoon_df):
 
   mecab = Mecab()
   webtoon_df['overview_token'] = webtoon_df['overview'].apply(lambda x: mecab.nouns(x))
   webtoon_df['overview_literal'] = webtoon_df['overview_token'].apply(lambda x: (' ').join(x))
   count_vect = TfidfVectorizer(min_df=0, ngram_range=(1, 2))
-  genre_mat = count_vect.fit_transform(webtoon_df['overview_literal'])
+  token_mat = count_vect.fit_transform(webtoon_df['overview_literal'])
 
-  genre_sim = cosine_similarity(genre_mat, genre_mat)
+  token_sim = cosine_similarity(token_mat, token_mat)
 
-  genre_sim_sorted_ind = genre_sim.argsort()[:, ::-1]
+  token_sim_sorted_ind = token_sim.argsort()[:, ::-1]
 
-  return genre_sim_sorted_ind
+  return token_sim_sorted_ind
 
-def find_sim_movie_ver2(df, sorted_ind, title_name, top_n=10):
-  title_movie = df[df['webtoon_name'] == title_name]
-  title_index = title_movie.index.values
+# 유사도 계산
+def find_sim_movie_ver2(df, sorted_ind, title_webtoon, top_n=10):
+  title_webtoon = df[df['webtoon_name'] == title_webtoon]
+  title_index = title_webtoon.index.values
 
   similar_indexes = sorted_ind[title_index, :(top_n*2)]
   similar_indexes = similar_indexes.reshape(-1)
@@ -52,7 +55,3 @@ def find_sim_movie_ver2(df, sorted_ind, title_name, top_n=10):
   similar_indexes = similar_indexes[similar_indexes != title_index]
 
   return df.iloc[similar_indexes][:top_n][['webtoon_name', 'overview', 'webtoon_writer', 'thumbnail_url', 'webtoon_score', 'webtoon_link', 'webtoon_platform', 'serialized_day']].to_json(orient='records', force_ascii=False)
-
-
-# def reults(title):
-#   similar_movies = find_sim_movie_ver2(webtoon_df, genre_sim_sorted_ind, '{}'.format(title), 10)
