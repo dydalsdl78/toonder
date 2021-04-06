86172cde9be34be82d15b7c91ccad6a656b148ed
jun9395 <jun9395@naver.com>
2021-04-07 00:50:45 +0900

    [FE+] ADD an env file for security

--- frontend/src/modules/auth.api.js
+++ frontend/src/modules/auth.api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 // for local test
-const API_URL = "http://127.0.0.1:8000/";
+const API_URL = process.env.REACT_APP_SERVER_URL;
 // for release
 // const API_URL = "";
 
 const join = (username, email, password, passwordConfirmation) => {
-  return axios.post(API_URL + "accounts/signup/", {
+  return axios.post(`${API_URL}/accounts/signup/`, {
     username,
     email,
     password,
@@ -17,7 +17,7 @@ const join = (username, email, password, passwordConfirmation) => {
 
 const login = (email, password) => {
   return axios
-    .post(API_URL + "accounts/api-token-auth/", {
+    .post(`${API_URL}/accounts/api-token-auth/`, {
       email,
       password,
     })
@@ -32,7 +32,7 @@ const login = (email, password) => {
 };
 
 const getuser = (token) => {
-  return axios.get(API_URL + "accounts/get_userinfo/", {
+  return axios.get(`${API_URL}/accounts/get_userinfo/`, {
     headers: {
       Authorization: `JWT ${token}`,
     },
@@ -51,7 +51,7 @@ const recomm_overall = async () => {
     },
   };
   console.log(config);
-  const res = await axios.get(API_URL + "recommends/recomm_overall/", config);
+  const res = await axios.get(`${API_URL}/recommends/recomm_overall/`, config);
   console.log(res);
   return res;
 };
--- frontend/src/modules/recommend.api.js
+++ frontend/src/modules/recommend.api.js
@@ -1,48 +1,48 @@
 import axios from "axios";
 
 // for local test
-const API_URL = "http://127.0.0.1:8000/recommends/";
+const API_URL = process.env.REACT_APP_SERVER_URL;
 // for release
 // const API_URL = "";
 
 const getAllRecommend = () => {
-  return axios.get(API_URL + "recomm_overall/");
+  return axios.get(`${API_URL}/recommends/recomm_overall/`);
 };
 
 const getGenreRecommend = () => {
-  return axios.get(API_URL + "recomm_genre/");
+  return axios.get(`${API_URL}/recommends/recomm_genre/`);
 };
 
 const getArtistRecommend = () => {
-  return axios.get(API_URL + "recomm_artist/");
+  return axios.get(`${API_URL}/recommends/recomm_artist/`);
 };
 
 const getSummaryRecommend = () => {
-  return axios.get(API_URL + "recomm_summary/");
+  return axios.get(`${API_URL}/recommends/recomm_summary/`);
 };
 
 const getScoreRecommend = () => {
-  return axios.get(API_URL + "recomm_score/");
+  return axios.get(`${API_URL}/recommends/recomm_score/`);
 };
 
 const getMediaRecommend = () => {
-  return axios.get(API_URL + "recomm_media/");
+  return axios.get(`${API_URL}/recommends/recomm_media/`);
 };
 
 const getRandomRecommend = () => {
-  return axios.get(API_URL + "recomm_random/");
+  return axios.get(`${API_URL}/recommends/recomm_random/`);
 };
 
 const getOppositionRecommend = () => {
-  return axios.get(API_URL + "recomm_opposition/");
+  return axios.get(`${API_URL}/recommends/recomm_opposition/`);
 };
 
 const getLikes = () => {
-  return axios.get(API_URL + "like/");
+  return axios.get(`${API_URL}/recommends/like/`);
 };
 
 const getFavs = () => {
-  return axios.get(API_URL + "favorite/");
+  return axios.get(`${API_URL}/recommends/favorite/`);
 };
 
 
