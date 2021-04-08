1de4195bb6f8590d3ea2f99359c3d8ac93a72966
dydalsdl78 <dydalsdl78@gmail.com>
2021-04-08 15:07:38 +0900

    [BE+] Add recommend image style

--- backend_main/recommends/urls.py
+++ backend_main/recommends/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import WebtoonOverAllViewSet, LikeViewSet, FavoriteViewSet, LikeListViewSet, FavoriteListViewSet
+from .views import WebtoonOverAllViewSet, WebtoonStyleViewSet, LikeViewSet, FavoriteViewSet, LikeListViewSet, FavoriteListViewSet
 
 urlpatterns = [
 
     # 웹툰 추천 카드 주소
     path('recomm_overall/', WebtoonOverAllViewSet.as_view({"get":"recomm_overall"})),
+    path('recomm_style/', WebtoonStyleViewSet.as_view({"get":"recomm_style"})),
 
     # 유저 좋아요, 찜목록 리스트 관련 주소들
     path('like/<int:webtoon_number>/', LikeViewSet.as_view({"post": "like"}), name='like'),
